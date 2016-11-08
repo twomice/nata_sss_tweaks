bcfb10c92c3fd6212203c86b94c92322695776a1
Allen Shaw <allen@JoineryHQ.com>
2016-11-08 17:19:27 -0600

    Bug-fix: onpage "no" warnings were not styled, and not showing on page refresh.

--- js/nata_sss_tweaks_webform_step1.js
+++ js/nata_sss_tweaks_webform_step1.js
@@ -1,22 +1,21 @@
 (function ($) {
   Drupal.behaviors.exampleModule = {
     attach: function (context, settings) {
-      console.log(settings.nata_sss_tweaks.onpage_validate_yes_formkeys)
       for (i in settings.nata_sss_tweaks.onpage_validate_yes_formkeys) {
-        $('input[type="radio"][name$="' + settings.nata_sss_tweaks.onpage_validate_yes_formkeys[i] + ']"]').change(nata_sss_onpage_validate_yes)
+        $('input[type="radio"][name$="' + settings.nata_sss_tweaks.onpage_validate_yes_formkeys[i] + ']"]').change(nata_sss_onpage_validate_yes);
+        $('input[type="radio"][name$="' + settings.nata_sss_tweaks.onpage_validate_yes_formkeys[i] + ']"]').change();
       }
     }
   };
 
   var nata_sss_onpage_validate_yes = function(e) {
     var radio = $(this);
-    if (radio.val() == 0) {
+    if (radio.val() == 0 && radio.is(':checked')) {
       radio.siblings('label').append('<div class="nata_sss_onpage_validate_yes_warning">' + Drupal.t('Note: This action is required for the Safe Sports School award.') + '</div>');
     }
     else {
       var name = radio.attr('name');
       $('input[type="radio"][name="' + name + '"][value="0"]').siblings('label').find('div.nata_sss_onpage_validate_yes_warning').remove();
-      console.log('input[type="radio"][name="' + name + '"][value="0"] div.nata_sss_onpage_validate_yes_warning');
     }
   }
 }(jQuery));
