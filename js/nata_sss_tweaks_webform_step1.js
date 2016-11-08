d724d187d429c3bdb0294c1c15784135c37a11dc
Allen Shaw <allen@JoineryHQ.com>
2016-11-08 10:23:17 -0600

    Added support for renewals:
    - Include membership ID in membership receipt.
    - Accept membership ID as evidence of existing membership.

--- js/nata_sss_tweaks_webform_step1.js
+++ js/nata_sss_tweaks_webform_step1.js
@@ -11,12 +11,12 @@
   var nata_sss_onpage_validate_yes = function(e) {
     var radio = $(this);
     if (radio.val() == 0) {
-      radio.siblings('label').append('<div class="nata_sss_pongage_validate_yes_warning">' + Drupal.t('Note: This action is required for the Safe Sports School award.') + '</div>');
+      radio.siblings('label').append('<div class="nata_sss_onpage_validate_yes_warning">' + Drupal.t('Note: This action is required for the Safe Sports School award.') + '</div>');
     }
     else {
       var name = radio.attr('name');
-      $('input[type="radio"][name="' + name + '"][value="0"]').siblings('label').find('div.nata_sss_pongage_validate_yes_warning').remove();
-      console.log('input[type="radio"][name="' + name + '"][value="0"] div.nata_sss_pongage_validate_yes_warning');
+      $('input[type="radio"][name="' + name + '"][value="0"]').siblings('label').find('div.nata_sss_onpage_validate_yes_warning').remove();
+      console.log('input[type="radio"][name="' + name + '"][value="0"] div.nata_sss_onpage_validate_yes_warning');
     }
   }
 }(jQuery));
